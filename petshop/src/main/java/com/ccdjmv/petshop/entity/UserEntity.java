610e2105fdd63a011eb1e27af28bf6c10f7bb9e6
Anonimos0703 <williamsevenial2003@gmail.com>
2024-12-01 23:52:44 +0800

    update

--- petshop/src/main/java/com/ccdjmv/petshop/entity/UserEntity.java
+++ petshop/src/main/java/com/ccdjmv/petshop/entity/UserEntity.java
@@ -1,66 +1,87 @@
-    package com.ccdjmv.petshop.entity;
+package com.ccdjmv.petshop.entity;
 
-    import java.util.List;
+import java.util.List;
 
-    import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-    import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-    import jakarta.persistence.CascadeType;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
-    @CrossOrigin(origins = "http://localhost:5173")
-    @Entity
-    @Table(name = "users")
-    public class UserEntity {
+@CrossOrigin(origins = "http://localhost:5173")
+@Entity
+@Table(name = "users")
+public class UserEntity {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    private String role;
     
-<<<<<<< HEAD
     @Column(columnDefinition = "LONGTEXT")
     private String profileImage;
     
-    @OneToMany
-    @JsonManagedReference
-    private List<AppointmentEntity> appointment;
-=======
-        @Column(name = "username", nullable = false, unique = true)
-        private String username;
->>>>>>> main
-    
-        @Column(name = "first_name", nullable = false)
-        private String firstName;
-    
-<<<<<<< HEAD
-    public UserEntity() {}
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    @JsonManagedReference("user-cart")
+    private CartEntity cart;
 
-    public UserEntity(String username, String firstName, String lastName, String email, String password,
-    		String role, String profileImage) {
-        this.username = username;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-        this.profileImage = profileImage;
-    }
+    // One-to-Many relationship with AddressEntity
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @JsonManagedReference("user-address")
+    private List<AddressEntity> addresses;  // Add the List of AddressEntities
     
-    
-    public String getProfileImage() {
-    	return profileImage;
-    }
-    public void setProfileImage(String profileImage) {
-    	this.profileImage = profileImage;
-    }
-    public Long getId() {
+    public UserEntity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserEntity(Long id, String username, String firstName, String lastName, String email, String password,
+			String role, CartEntity cart, List<AddressEntity> addresses, String profileImage) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.cart = cart;
+		this.addresses = addresses;
+		this.profileImage = profileImage;
+	}
+	
+	public String getProfileImage() {
+		return profileImage;
+	}
+	public void setProfileImage(String profileImage) {
+		this.profileImage = profileImage;
+	}
+
+	public Long getId() {
         return id;
     }
 
@@ -78,7 +99,7 @@ import jakarta.persistence.Table;
 
     public String getFirstName() {
         return firstName;
-    }	
+    }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
@@ -115,100 +136,8 @@ import jakarta.persistence.Table;
     public void setRole(String role) {
         this.role = role;
     }
-=======
-        @Column(name = "last_name", nullable = false)
-        private String lastName;
->>>>>>> main
     
-        @Column(name = "email", nullable = false, unique = true)
-        private String email;
-    
-        @Column(name = "password", nullable = false)
-        private String password;
-    
-        @Column(name = "role", nullable = false)
-        private String role;
-    
-        // One-to-Many relationship with AddressEntity
-        @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-        @JsonManagedReference
-        private List<AddressEntity> addresses;  // Add the List of AddressEntities
-    
-        // Getter and Setter for addresses
-        public List<AddressEntity> getAddresses() {
-            return addresses;
-        }
-    
-        public void setAddresses(List<AddressEntity> addresses) {
-            this.addresses = addresses;
-        }
-    
-        // Getter and Setter methods for other fields
-        public Long getId() {
-            return id;
-        }
-    
-        public void setId(Long id) {
-            this.id = id;
-        }
-    
-        public String getUsername() {
-            return username;
-        }
-    
-        public void setUsername(String username) {
-            this.username = username;
-        }
-    
-        public String getFirstName() {
-            return firstName;
-        }
-    
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
-    
-        public String getLastName() {
-            return lastName;
-        }
-    
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
-    
-        public String getEmail() {
-            return email;
-        }
-    
-        public void setEmail(String email) {
-            this.email = email;
-        }
-    
-        public String getPassword() {
-            return password;
-        }
-    
-        public void setPassword(String password) {
-            this.password = password;
-        }
-    
-        public String getRole() {
-            return role;
-        }
-    
-        public void setRole(String role) {
-            this.role = role;
-        }
-        
-        
-    }
-<<<<<<< HEAD
-    
-    public void setAppointment(List<AppointmentEntity> appointment) {
-    	this.appointment = appointment;
-    }
-
-	public CartEntity getCart() {
+    public CartEntity getCart() {
 		return cart;
 	}
 
@@ -216,9 +145,13 @@ import jakarta.persistence.Table;
 		this.cart = cart;
 	}
 
-	
-    
-    
+	// Getter and Setter for addresses
+    public List<AddressEntity> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<AddressEntity> addresses) {
+        this.addresses = addresses;
+    }
+
 }
-=======
->>>>>>> main
