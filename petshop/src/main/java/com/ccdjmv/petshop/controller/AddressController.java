f21a7f9ab6833b6cfe7a698da7e2aea31c93a008
Matt Jude Augustine Getigan <mattjudegetigan10@gmail.com>
2024-12-01 21:36:11 +0800

    Added Address Entity

---
+++ petshop/src/main/java/com/ccdjmv/petshop/controller/AddressController.java
@@ -0,0 +1,60 @@
+package com.ccdjmv.petshop.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ccdjmv.petshop.entity.AddressEntity;
+import com.ccdjmv.petshop.service.AddressService;
+
+@RestController
+@RequestMapping("/addresses")
+public class AddressController {
+
+    @Autowired
+    private AddressService addressService;
+
+    // Add a new address for a user
+    @PostMapping("/users/{userId}")
+    public ResponseEntity<AddressEntity> addAddress(@PathVariable Long userId, @RequestBody AddressEntity address) {
+        AddressEntity createdAddress = addressService.addAddressToUser(userId, address);
+        if (createdAddress != null) {
+            return new ResponseEntity<>(createdAddress, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    // Get all addresses for a user
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<List<AddressEntity>> getAddresses(@PathVariable Long userId) {
+        List<AddressEntity> addresses = addressService.getAddressesByUserId(userId);
+        return ResponseEntity.ok(addresses);
+    }
+
+    // Update an address
+    @PutMapping("/users/{userId}/{addressId}")
+    public ResponseEntity<AddressEntity> updateAddress(@PathVariable Long userId, @PathVariable Integer addressId, @RequestBody AddressEntity address) {
+        AddressEntity updatedAddress = addressService.updateAddress(userId, addressId, address);
+        if (updatedAddress != null) {
+            return ResponseEntity.ok(updatedAddress);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    // Delete an address
+    @DeleteMapping("/users/{userId}/{addressId}")
+    public ResponseEntity<Void> deleteAddress(@PathVariable Long userId, @PathVariable Integer addressId) {
+        addressService.deleteAddress(userId, addressId);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ petshop/src/main/java/com/ccdjmv/petshop/entity/AddressEntity.java
@@ -0,0 +1,112 @@
+package com.ccdjmv.petshop.entity;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@Entity
+@Table(name = "addresses")
+public class AddressEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer addressId;
+
+    @Column(name = "region", nullable = false)
+    private String region;
+
+    @Column(name = "province", nullable = false)
+    private String province;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "barangay", nullable = false)
+    private String barangay;
+
+    @Column(name = "postalCode", nullable = false)
+    private String postalCode;
+
+    @Column(name = "buildingHouseNo", nullable = false)
+    private String buildingHouseNo;
+
+    // Many-to-One relationship with UserEntity
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user; // User that owns this address
+
+    // Getter and Setter methods for Address fields
+    public Integer getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Integer addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getBarangay() {
+        return barangay;
+    }
+
+    public void setBarangay(String barangay) {
+        this.barangay = barangay;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getBuildingHouseNo() {
+        return buildingHouseNo;
+    }
+
+    public void setBuildingHouseNo(String buildingHouseNo) {
+        this.buildingHouseNo = buildingHouseNo;
+    }
+
+    // Getter and Setter for user (Many-to-One relationship with UserEntity)
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+}
